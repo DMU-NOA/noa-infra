1cdac3aa4e54af9e637ff51f5e85181e8c20cda3
dazigeum <96718120+handahyun@users.noreply.github.com>
2026-06-01 00:55:48 +0900

    feat(s3): main에 s3 모듈 추가

--- main.tf
+++ main.tf
@@ -12,3 +12,9 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# S3 모듈 - 프론트엔드 정적 파일 버킷
+module "s3" {
+  source = "./modules/s3"
+  frontend_bucket_name = var.frontend_bucket_name
+}
