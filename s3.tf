44bd15680b7a68ecb1991b12264b5ca1e7702d16
dazigeum <96718120+handahyun@users.noreply.github.com>
2026-05-31 23:06:52 +0900

    feat(s3): 버킷 퍼블릭 액세스 차단 추가

--- s3.tf
+++ s3.tf
@@ -2,11 +2,12 @@ resource "aws_s3_bucket" "frontend" {
   bucket = var.frontend_bucket_name
 }
 
-# resource "aws_s3_bucket_public_access_block" "frontend" {
-#   bucket = aws_s3_bucket.frontend.id
-#
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+# 버킷 퍼블릭 액세스 차단
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true # 퍼블릭 ACL 설정 차단
+  block_public_policy     = true # 퍼블릭 버킷 정책 차단
+  ignore_public_acls      = true # 기존 퍼블릭 ACL 무시
+  restrict_public_buckets = true # 퍼블릭 버킷 접근 제한
+}
