c8e7db5c7818dcb61eb0566625b419271cf369f2
mooonu <moonudev@gmail.com>
2026-06-02 21:28:45 +0900

    feat(s3): add s3 module variables and outputs

---
+++ modules/s3/outputs.tf
@@ -0,0 +1,11 @@
+# 버킷 ID - 다른 리소스(CloudFront OAC 등)에서 버킷을 참조할 때 사용
+output "bucket_id" {
+  description = "프론트엔드 S3 버킷 ID"
+  value       = aws_s3_bucket.frontend.id
+}
+
+# 버킷 ARN - IAM 정책에서 버킷 접근 권한 부여 시 사용
+output "bucket_arn" {
+  description = "프론트엔드 S3 버킷 ARN"
+  value       = aws_s3_bucket.frontend.arn
+}
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,3 +1,4 @@
 variable "frontend_bucket_name" {
-  type = string
+  type        = string
+  description = "프론트엔드 정적 파일을 서빙할 S3 버킷명"
 }
